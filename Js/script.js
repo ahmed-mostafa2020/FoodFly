0fe696b1ac0de4b29b832f6a3ef0d554943a7191
Ahmed Mostafa <ahmed-mostafa2020>
2022-11-28 16:50:54 +0200

    first upload

---
+++ Js/script.js
@@ -0,0 +1,23 @@
+/*global $ , alert, console*/ 
+
+$(function(){
+    'use strict';
+    // $('html').niceScroll();
+
+    //close side-nav
+    $('.navbar .side-nav .side-nav-header').on('click','.close-menu',function(){
+        $('.navbar .side-nav').css('left','-400px');
+    });
+
+    //
+
+
+
+
+
+});
+
+
+// $(document).ready(function(){
+    
+// });
