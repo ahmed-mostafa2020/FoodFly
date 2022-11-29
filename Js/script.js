48dfeb8ef62f5b1698f23b9dbba3f99c7a7cb2fb
Ahmed Mostafa <ahmed-mostafa2020>
2022-11-29 04:24:14 +0200

    2sec left

--- Js/script.js
+++ Js/script.js
@@ -4,10 +4,27 @@ $(function(){
     'use strict';
     // $('html').niceScroll();
 
+    // open & close side-nav from burger icon
+    $('.navbar nav .main-nav ').on('click','.burger-icon',function(){
+
+        let  sidebar = $('.navbar .side-nav');
+        if(sidebar.hasClass('block')){
+
+            sidebar.css('left','-400px');
+            sidebar.removeClass('block');
+        }
+        else{
+            sidebar.css('left','0');
+            sidebar.addClass('block');
+        }
+    });
+
     //close side-nav
     $('.navbar .side-nav .side-nav-header').on('click','.close-menu',function(){
         $('.navbar .side-nav').css('left','-400px');
     });
+
+
 
     //         to show instgram icon 
     // $('.inst .box').hover(function(){
