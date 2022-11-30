70e315b277f48f4508f7be2a6196e718e67fde7e
Ahmed Mostafa <ahmed-mostafa2020>
2022-11-30 23:44:34 +0200

    owlleft

--- Js/script.js
+++ Js/script.js
@@ -4,6 +4,26 @@ $(function(){
     'use strict';
     // $('html').niceScroll();
 
+    //calling owl plugin
+    // $(".owl-carousel").owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+
     // open & close side-nav from burger icon
     $('.navbar nav .main-nav ').on('click','.burger-icon',function(){
 
@@ -36,9 +56,9 @@ $(function(){
             $('.tabes .cards .row').css('height','auto');
         }
         else {
-            $('.tabes .cards .row').css('height','480px');
+            $('.tabes .cards .row').css('min-height','480px');
 
-            $('.tabes .cards div.card-body').not($($(this).data('content'))).parent().fadeOut(400);
+            $('.tabes .cards div.card-body').not($($(this).data('content'))).parent().fadeOut(400).delay(300);
             $($(this).data('content')).parent().fadeIn(500);
           }
 
